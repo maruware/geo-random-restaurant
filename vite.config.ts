4d8a24947dc0ae01290ea464bde54eb1fa31f0e1
maruware <me@maruware.com>
2026-01-22 02:23:37 +0900

    fixup! feat: Support PWA

--- vite.config.ts
+++ vite.config.ts
@@ -24,12 +24,12 @@ export default defineConfig(({ command }) => {
           start_url: "/",
           icons: [
             {
-              src: "/android-chrome-192x192.png",
+              src: "android-chrome-192x192.png",
               sizes: "192x192",
               type: "image/png",
             },
             {
-              src: "/android-chrome-512x512.png",
+              src: "android-chrome-512x512.png",
               sizes: "512x512",
               type: "image/png",
               purpose: "any maskable",
@@ -41,7 +41,7 @@ export default defineConfig(({ command }) => {
           runtimeCaching: [
             {
               urlPattern: /^https:\/\/maps\.googleapis\.com\/.*/i,
-              handler: "CacheFirst",
+              handler: "NetworkFirst",
               options: {
                 cacheName: "google-maps-cache",
                 expiration: {
